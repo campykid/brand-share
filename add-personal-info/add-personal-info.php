41cd41d9c6344d07919cbb0e2c6087d8bd60bbd5
vladislav khvostov <vladkhvo@gmail.com>
2015-04-05 09:19:11 +0300

    Переименовывает файлы, фиксит баг с относительным путем.

--- add-personal-info/add-personal-info.php
+++ add-personal-info/add-personal-info.php
@@ -4,7 +4,7 @@
 
 /*
 
-Plugin Name: addPersonalInfo
+Plugin Name: add-personal-info
 
 Description: Подключает JS файл со скриптом в котором описана логика отображения персональных данных (фото и ссылки на соц сети)
 
@@ -26,9 +26,9 @@ function addScript ()
 
 	echo "
 	// Атрибут asunc запрещает браузеру блокировать загрузку страницы, если скрипт не грузится 
-	<script async src='wp-content/plugins/addPersonalInfo/addPersonalInfo.js'></script>
+	<script async src='http://vlad-khvostov.ru/wp-content/plugins/add-personal-info/add-personal-info.js'></script>
 
-	<link rel='stylesheet' href='wp-content/plugins/addPersonalInfo/addPersonalInfo.css'>
+	<link rel='stylesheet'http://vlad-khvostov.ru/wp-content/plugins/add-personal-info/add-personal-info.css'>
 
 
 
